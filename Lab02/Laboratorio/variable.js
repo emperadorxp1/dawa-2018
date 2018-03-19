e53a7231f1eb57072ad145ec632d0dea2970547f
emperadorxp1 <joxua.1995@gmail.com>
2018-03-19 14:51:59 -0500

    lab02

---
+++ Lab02/Laboratorio/variable.js
@@ -0,0 +1,33 @@
+var http = require('http'), fs = require('fs')
+var parametros =[],valores = [],arreglo_parametros=[]
+
+http.createServer(function(req,res){
+    fs.readFile('./form.html',function(err,html){
+        var html_string = html.toString()
+
+        if(req.url.indexOf('?')>0){
+            var url_data =req.url.split('?')
+            arreglo_parametros = url_data[1].split('&')
+        }
+
+        for(var i=0;i<arreglo_parametros.length;i++){
+            var parametro = arreglo_parametros[i]
+            var param_data = parametro.split('=')
+            parametros[i] = param_data[0]
+            valores[i] = param_data[1]
+        }
+
+        for(var i=0;i<parametros.length;i++){
+            html_string = html_string.replace('{'+parametros[i]+'}',valores[i])
+        }
+
+        console.log(parametros)
+        console.log(valores)
+        console.log(arreglo_parametros)
+        
+        res.writeHead(200,{'Content-type':'text/html'})
+        res.write(html_string)
+        res.end()
+    })
+})
+.listen(8080)
