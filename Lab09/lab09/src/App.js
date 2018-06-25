ab7b0db5410a7232c98b72888df85d066085c0ae
emperadorxp1 <joxua.1995@gmail.com>
2018-06-24 19:03:40 -0500

    lab09

--- Lab09/lab09/src/App.js
+++ Lab09/lab09/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+
 import Calculadora from './components/Calculadora/Calculadora';
 
-class App extends Component{
-  render(){
-    return(<div>
-    <Calculadora valor={5}/>
-    <Calculadora valor1={15}/>
-    </div>)
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Calculadora />
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
